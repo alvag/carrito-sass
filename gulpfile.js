0aec4fd2b16f627d223fce3f516f39e64506aef1
Max Alva <alva85@gmail.com>
2019-01-14 20:11:57 -0500

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+const gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('sass', () => {
+    gulp.src('scss/app.scss')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(sass({
+            includePaths: ['scss']
+        }));
+});
+
+gulp.task('watch', ['sass'], () => {
+    gulp.watch(['scss/*.scss'], ['sass']);
+});
