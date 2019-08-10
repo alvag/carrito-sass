cb5125340aca518159777fed55b661849b96692d
Max Alva <alva85@gmail.com>
2019-08-10 18:16:22 -0500

    Listado de cursos

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,19 @@
-var { watch, series, src, dest } = require('gulp');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
+let { watch, series, src, dest } = require( 'gulp' );
+let sass = require( 'gulp-sass' );
+let autoprefixer = require( 'gulp-autoprefixer' );
 
 function sassAndAuto() {
-    return src('scss/app.scss')
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(sass({
-            includePaths: ['scss']
-        }))
-        .pipe(dest('css'));
+    return src( 'scss/app.scss' )
+    .pipe( autoprefixer( {
+        browsers: ['last 2 versions'],
+        cascade: false
+    } ) )
+    .pipe( sass( {
+        includePaths: ['scss']
+    } ) )
+    .pipe( dest( 'css' ) );
 }
 
 exports.sassAndAuto = sassAndAuto;
-exports.default = series(sassAndAuto);
-watch(['scss/*.scss'], series(sassAndAuto));
+exports.default = series( sassAndAuto );
+watch( ['scss/*.scss'], series( sassAndAuto ) ); 
